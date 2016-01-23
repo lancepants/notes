7cd38b417474cb2d2a7e7b236a5184f9e6f18cce
Lance Laursen <llaursen@rubiconproject.com>
2016-01-22 18:34:14 -0800

    update

--- lern/minmax.py
+++ lern/minmax.py
@@ -12,24 +12,59 @@ So, you'll need to have several functions:
     min(arg1, arg2, *args[, key])
 '''
 def max(iterable, key=None):
+  curmax=iterable[0]
   print(iterable)
   if key:
-    print(key(2))
-
-
+    dicky = {}
+    # Generate a dict where dict key is keyfn(iteritem) and
+    # dict key value is the input list index location
+    for index in range(len(iterable)):
+      dicky[key(iterable[index])] = index
+    for i in dicky:
+      if i > curmax:
+        curmax = i
+    print(iterable[dicky[curmax]])
+    return iterable[dicky[curmax]]
+  else:
+    for i in iterable:
+      if i > curmax:
+          curmax = i
+  print(curmax)
+  return curmax
 
 def min(iterable, key=None):
-  print("min")
+  curmin=iterable[0]
+  print(iterable)
+  if key:
+    dicky = {}
+    # Generate a dict where dict key is keyfn(iteritem) and
+    # dict key value is the input list index location
+    for index in range(len(iterable)):
+      dicky[key(iterable[index])] = index
+    for i in dicky:
+      if i < curmin:
+        curmin = i
+    print(iterable[dicky[curmin]])
+    return iterable[dicky[curmin]]
+  else:
+    for i in iterable:
+      if i < curmin:
+          curmin = i
+  print(curmin)
+  return curmin
+
 
 if __name__ == '__main__':
   max([1,2,3])
-  max([1,2,3], key=lambda x: x**2)
-#    max(3, 2)
-#    min(3, 2)
-#    max([1, 2, 0, 3, 4])
-#    min("hello")
-#    max(2.2, 5.6, 5.9, key=int)
-#    min([[1,2], [3, 4], [9, 0]], key=lambda x: x[1])
+  max([1,2,4,3,4], key=lambda x: x**2)
+  min([1,2,3])
+  min([1,2,4,3,4], key=lambda x: x**2)
+#  max(3, 2)
+#  min(3, 2)
+  max([1, 2, 0, 3, 4])
+  min("hello")
+#  max(2.2, 5.6, 5.9, key=int)
+  min([[1,2], [3, 4], [9, 0]], key=lambda x: x[1])
 
 '''
 max(3, 2) == 3
