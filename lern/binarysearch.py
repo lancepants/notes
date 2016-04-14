58ce859da4e06f76fccd5c35b98b666d501d60d9
lancepants <derp@derpy.com>
2016-04-13 22:15:53 -0700

    more notes

--- lern/binarysearch.py
+++ lern/binarysearch.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+'''
+The recursion solutions here rely on searching some global variable outside
+of their scope. The alternative to this is to continually pass a new smaller
+list up, but that is very space inefficient.
+
+'''
 import sys
 import random
 
@@ -52,6 +58,35 @@ def recurse(tries, guess, minval, maxval):
     guess = minval + ((maxval - minval)/2)
     recurse(tries,int(guess),minval,maxval)
 
+'''
+Alternately, not using recursion
+'''
+def binsearch(lookfor, listy):
+  middle = int((len(listy)-1)/2)
+  print(middle)
+  first = 0
+  last = len(listy)-1
+  itercatch = 0
+  while True:
+    # reset middle to first plus half of the new index range
+    middle = int((last - first)/2) + first
+    print('middle is now {}'.format(middle))
+    if itercatch > 2:
+      return False
+    if listy[middle] == lookfor:
+      print(middle)
+      return middle
+    elif listy[middle] > lookfor:
+      print('{} is greater than {}'.format(listy[middle], lookfor))
+      last = middle - 1
+      itercatch += 1
+    elif listy[middle] < lookfor:
+      print('{} is less than {}'.format(listy[middle], lookfor))
+      first = middle + 1
+      itercatch += 1
+
 if __name__ == '__main__':
+  binsearch(38, [2,7,12,13,19,27,38,99,102])
   recurse(1,500,1,1000)
+  #guessloop()  # interactive
 
